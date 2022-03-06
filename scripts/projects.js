646c509a52a149e91988d8b6b6e3904456731fb8
Jacob Lopez <jacoblopez.dev@gmail.com>
2022-03-06 14:41:59 -0600

    Updates

---
+++ scripts/projects.js
@@ -0,0 +1,26 @@
+export const PROJECT_LIST = [
+    {
+        title: "Cloudy",
+        description: "Cloudy is a fictional cloud hosting platform. Built with base HTML, CSS3, and JS. Cloudy serves as an example of my ability to design and develop simple websites.",
+        cover_image: "cloudy.png",
+        source_code: "https://github.com/JacobLopez-dev/cloudy",
+        live_link: "https://fervent-mayer-b0de3f.netlify.app/index.html"
+
+    },
+    {
+        title: "Dynamic Homepage",
+        description: "This project is meant to be the homepage when opening a browser. Local storage allows the user to save their name for a personalized greeting, a section for today's focus and a todo list. It also integrates a weather api and displays current weather based on users location.",
+        cover_image: "dynamic.png",
+        source_code: "https://github.com/JacobLopez-dev/dynamicHomePage",
+        live_link: "https://gallant-poitras-aacec5.netlify.app/"
+
+    },
+    {
+        title: "Typing Game",
+        description: "This is a simple typing game which tests your speed an accuracy to determine a typing score. The project is built with basic html, css, and vanilla JS. Utilitzed local storage to keep track of highscores. Also features a dark mode!",
+        cover_image: "typing_game.png",
+        source_code: "https://github.com/JacobLopez-dev/typing_game",
+        live_link: "https://infallible-mestorf-32c495.netlify.app"
+
+    }
+]
