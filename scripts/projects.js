b7158723627dc8d49799274eb926443113f9d0e0
Jacob Lopez <jacoblopez.dev@gmail.com>
2022-08-08 00:58:50 -0500

    Updates

--- scripts/projects.js
+++ scripts/projects.js
@@ -1,10 +1,10 @@
 export const PROJECT_LIST = [
     {
-        title: "Cloudy",
-        description: "Cloudy is a fictional cloud hosting platform. Built with base HTML, CSS3, and JS. Cloudy serves as an example of my ability to design and develop simple websites.",
-        cover_image: "cloudy.png",
-        source_code: "https://github.com/JacobLopez-dev/cloudy",
-        live_link: "https://fervent-mayer-b0de3f.netlify.app/index.html"
+        title: "Crypto Top Ten",
+        description: "CTT is a full stack MERN web app. It was created to be a simple platform that introduces people to the world of Crypto. It features CRUD operations, user authentication/authorization, interacts with multiple APIs, and Amazon AWS S3 as a second storage solution.",
+        cover_image: "cryptoApp.png",
+        source_code: "https://github.com/JacobLopez-dev/cryptoTop10",
+        live_link: "https://crypto-top-10.herokuapp.com/"
 
     },
     {
@@ -16,11 +16,18 @@ export const PROJECT_LIST = [
 
     },
     {
+        title: "Cloudy",
+        description: "Cloudy is a fictional cloud hosting platform. This serves as an example of my ability to design and develop simple websites using semantic HTML, modern CSS, and vanilla JS.",
+        cover_image: "cloudy.png",
+        source_code: "https://github.com/JacobLopez-dev/cloudy",
+        live_link: "https://fervent-mayer-b0de3f.netlify.app/index.html"
+
+    },
+    {
         title: "Typing Game",
         description: "This is a simple typing game which tests your speed an accuracy to determine a typing score. The project is built with basic html, css, and vanilla JS. Utilitzed local storage to keep track of highscores. Also features a dark mode!",
         cover_image: "typing_game.png",
         source_code: "https://github.com/JacobLopez-dev/typing_game",
         live_link: "https://infallible-mestorf-32c495.netlify.app"
-
     }
 ]
